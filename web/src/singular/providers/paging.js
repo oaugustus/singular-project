1af4cd9128d175df89b5e0d629cf157b725fa449
Otávio Fernandes <oaugustus@gmail.com>
2018-07-25 17:43:44 -0300

    Criado provedor de serviços de paginação

--- web/src/singular/providers/paging.js
+++ web/src/singular/providers/paging.js
@@ -20,9 +20,23 @@
         this.$get = [function(){
             function pagingFactory(){
                 var Paging = {
-
-
+                    isPagingProvider: true,
+                    limit: 100,
+                    options: [10,25,50,100,500],
+                    page: 1,
+                    total: 0,
+                    /**
+                     * Função que recupera a paginação a ser enviada para o backend.
+                     */
+                    getPaging: getPagingFn
                 };
+
+                function getPagingFn() {
+                    return {
+                        start: (Paging.page -1) * Paging.limit,
+                        limit: Paging.limit
+                    }
+                }
 
                 return Paging;
             }
